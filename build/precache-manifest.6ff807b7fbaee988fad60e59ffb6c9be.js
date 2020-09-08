087f1f811c02612bddd7ef86637370a2073b9458
JONGMYEONG JEONG <jmjeong1112@gmail.com>
2020-09-08 11:50:01 +0900

    Build folder upload

---
+++ build/precache-manifest.6ff807b7fbaee988fad60e59ffb6c9be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4372a3b2b7e5f5945241202dfe94cfb9",
+    "url": "/nomad_coders_movie_app/index.html"
+  },
+  {
+    "revision": "e1f69604e1ef3f037119",
+    "url": "/nomad_coders_movie_app/static/css/main.a164f2b2.chunk.css"
+  },
+  {
+    "revision": "4d5ad2302ebdbda60e6d",
+    "url": "/nomad_coders_movie_app/static/js/2.ee2a94a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_coders_movie_app/static/js/2.ee2a94a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1f69604e1ef3f037119",
+    "url": "/nomad_coders_movie_app/static/js/main.07c5d28f.chunk.js"
+  },
+  {
+    "revision": "ff575aaad91ecc2a6a35",
+    "url": "/nomad_coders_movie_app/static/js/runtime-main.83615f19.js"
+  }
+]);
